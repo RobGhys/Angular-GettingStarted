fd635808dbe7d0613c4405bed3cf81a6e0c28f20
Robin Ghyselinck <robin.ghyselinck2@kbc.be>
2021-12-07 08:29:03 +0100

    Refactored the modules

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,40 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule} from "@angular/common/http";
-import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
 
-import { ProductListComponent } from './products/product-list.component';
-import {ConvertToSpacesPipe} from "./shared/convert-to-spaces.pipe";
-import {StarComponent} from "./shared/star-component";
-import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 import {WelcomeComponent} from "./home/welcome.component";
 import {RouterModule} from "@angular/router";
-import {ProductDetailGuard} from "./products/product-detail.guard";
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      {
-        path: 'products',
-        component: ProductListComponent
-      },
-      {
-        path: 'products/:id',
-        canActivate: [ProductDetailGuard],
-        component: ProductDetailComponent
-      },
       {
         path: 'welcome',
         component: WelcomeComponent
@@ -49,7 +30,8 @@ import {ProductDetailGuard} from "./products/product-detail.guard";
         redirectTo: 'welcome',
         pathMatch: 'full'
       }
-    ])
+    ]),
+    ProductModule
   ],
   bootstrap: [
     AppComponent
